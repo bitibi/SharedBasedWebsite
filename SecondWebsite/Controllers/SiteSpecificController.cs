9cb18ea2b2414dc7550344bb789c8b08c2515d8e
Tibor Bicskei <tibor@azonetwork.com>
2018-01-27 19:37:22 +0000

    Create a Site specific View in the SecondWebsite

---
+++ SecondWebsite/Controllers/SiteSpecificController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SharedClassLibrary.Models;
+
+namespace SecondWebsite.Controllers
+{
+    public class SiteSpecificController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
